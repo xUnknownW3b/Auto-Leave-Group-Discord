a3a51499c3531c4db541f3a011e947a7b01c233d
xUnknown <jeankercier@gmail.com>
2024-05-27 19:34:26 +0200

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-# Replace "your_token" with your actual token
-TOKEN = "your_token"
-
-# Set the message you want to send in the group DMs
-MESSAGE = "❌ No groups without my permission! ❌"
